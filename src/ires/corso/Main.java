868dfa92364c46e60957a184395156a5765ffac3
Daw11 <davidebusterna@gmail.com>
2021-06-03 17:43:21 +0200

    Show the position of the max value

--- src/ires/corso/Main.java
+++ src/ires/corso/Main.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner in = new Scanner( System.in );
+        Scanner in = new Scanner( System.in );
 	    System.out.println("Inserisci il primo valore: ");
 	    Integer a = in.nextInt();
         System.out.println("Inserisci il secondo valore: ");
         Integer b = in.nextInt();
         System.out.println("Inserisci il terzo valore: ");
         Integer c = in.nextInt();
-        Integer maxVal;
+        Integer maxVal, maxPos;
 
         if( a > b ) {
             maxVal = a;
+            maxPos = 0;
         }
         else {
             maxVal = b;
+            maxPos = 1;
         }
 
-        if( c > maxVal )
+        if( c > maxVal ) {
             maxVal = c;
+            maxPos = 2;
+        }
 
-        System.out.println("Il valore massimo è: " + maxVal);
+        String[] positions = {"PRIMO", "SECONDO", "TERZO"};
+        System.out.println("Il numero maggiore che hai inserito è il " + positions[maxPos] + " e vale: " + maxVal);
     }
 }
